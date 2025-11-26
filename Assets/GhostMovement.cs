8a16f3e6f13580fa6aa44887621c180877621f59
gabby-m056 <gabriellemsmarks@outlook.com>
2025-11-26 09:46:26 +0000

    Update GhostMovement.cs

--- Assets/GhostMovement.cs
+++ Assets/GhostMovement.cs
@@ -11,6 +11,13 @@ public class GhostMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        if (agent.remainingDistance <= 1.0f)
+        {
+            float x = Random.range(-20.0f,20.0f);
+            float z =Random.range(-20.0f,20.0f);
+
+            agent.destination = new Vector3(x,0.0f,z);
+        }
     }
 }
